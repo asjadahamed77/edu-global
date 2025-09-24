5c9dfd12413432bcde71b1be3d0a9f8b1cdd1ea0
asjadahamed77 <ahamedasjad77@gmail.com>
2025-09-24 18:44:49 +0530

    Completed Sign in and Sign out by github

--- edu-global/app/(auth)/login/page.tsx
+++ edu-global/app/(auth)/login/page.tsx
@@ -1,89 +1,20 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { authClient } from "@/lib/auth-client";
-import { GithubIcon, Loader } from "lucide-react";
-import { useTransition } from "react";
-import { toast } from "sonner";
 
-export default function LoginPage() {
+import { LoginForm } from "./_components/LoginForm";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-    const [githubPending, startGithubTransition] = useTransition()
+export default async function LoginPage() {
 
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
 
-    async function signInWithGithub(){
-      startGithubTransition(
-        async ()=> {
-            await authClient.signIn.social({
-                provider: "github",
-                callbackURL: "/",
-                fetchOptions: {
-                    onSuccess: ()=> {
-                        toast.success('Signed in with Github, you will be redirected..')
-                    },
-                    onError: (error)=> {
-                        toast.error(error.error.message)
-                    }
-                }
-            })
-        }
-      )
+    if(session){
+        return redirect('/')
     }
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="text-xl">Welcome Back!</CardTitle>
-        <CardDescription>
-          Login with your GitHub or Email Account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col gap-4">
-        <Button disabled={githubPending} onClick={signInWithGithub} className="w-full" variant="outline">
-         {
-            githubPending ? (
-                <>
-                <Loader className="size-4 animate-spin" />
-                <span>Loading...</span>
-                </>
-            ) : (
-               <>
-                <GithubIcon className="size-4" /> 
-                Sign in with GitHub
-               </>
-            )
-         }
-        </Button>
-        <div className="relative flex items-center text-center text-sm">
-          <div className="flex-grow border-t border-border"></div>
-          <span className="relative z-10 bg-card px-2 text-muted-foreground">
-            or continue with
-          </span>
-          <div className="flex-grow border-t border-border"></div>
-        </div>
-
-    <div className="grid gap-3">
-    <div className="grid gap-2 ">
-        <Label htmlFor="email">
-            Email
-        </Label>
-        <Input type="email" placeholder="ajji@gmail.com" />
-    </div> 
-    </div>
-
-    <Button>
-        Continue with Email
-    </Button>
-
-      </CardContent>
-    </Card>
+   <LoginForm />
   );
 }
--- edu-global/app/page.tsx
+++ edu-global/app/page.tsx
@@ -1,9 +1,40 @@
-import {ThemeToggle} from '@/components/ui/themeToggle'
+"use client"
+import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-export default function Home() {
+export default  function Home() {
+
+  const router = useRouter()
+
+  const { 
+    data: session, 
+} = authClient.useSession() 
+
+ async function signOut(){
+  await authClient.signOut({
+    fetchOptions: {
+      onSuccess: ()=> {
+        router.push("/")
+        toast.success("Sign out Successfully")
+      }
+    }
+  })
+ }
+
   return (
-   <div>
- <ThemeToggle />
-   </div>
+    <div>
+      <ThemeToggle />
+      {session ? (
+        <div>
+          <p>{session.user.name}</p> 
+          <Button onClick={signOut}>
+            Logout
+          </Button>
+        </div>
+      ): <Button onClick={()=> router.push("/login")} >Login</Button>}
+    </div>
   );
 }
