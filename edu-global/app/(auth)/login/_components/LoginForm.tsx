13d375431a8e87f8d89eb842878c7fcaa24b154f
asjadahamed77 <ahamedasjad77@gmail.com>
2025-09-25 19:03:31 +0530

    Working in sending otp for the sign in with email

--- edu-global/app/(auth)/login/_components/LoginForm.tsx
+++ edu-global/app/(auth)/login/_components/LoginForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -11,81 +11,111 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { authClient } from "@/lib/auth-client";
-import { GithubIcon, Loader } from "lucide-react";
-import { useTransition } from "react";
+import { GithubIcon, Loader, Loader2, Send } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
 import { toast } from "sonner";
 
+export function LoginForm() {
+    const router = useRouter()
+  const [githubPending, startGithubTransition] = useTransition();
+  const [emailPending, startEmailTransition] = useTransition();
+  const [email, setEmail] = useState("")
 
-export function LoginForm(){
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirected..");
+          },
+          onError: (error) => {
+            toast.error(error.error.message);
+          },
+        },
+      });
+    });
+  }
 
-    const [githubPending, startGithubTransition] = useTransition()
-
-
-    async function signInWithGithub(){
-      startGithubTransition(
-        async ()=> {
-            await authClient.signIn.social({
-                provider: "github",
-                callbackURL: "/",
-                fetchOptions: {
-                    onSuccess: ()=> {
-                        toast.success('Signed in with Github, you will be redirected..')
-                    },
-                    onError: (error)=> {
-                        toast.error(error.error.message)
-                    }
+  function signInWithEmail() {
+    startEmailTransition(async ()=> {
+        await authClient.emailOtp.sendVerificationOtp({
+            email: email,
+            type: "sign-in",
+            fetchOptions: {
+                onSuccess: ()=> {
+                    toast.success("Email Sent")
+                    router.push(`/verify-request`)
+                },
+                onError: ()=>{
+                    toast.error("Error Occured in sending email")
                 }
-            })
-        }
-      )
-    }
+            }
+        })
+    })
 
-    return (
-        <Card>
-        <CardHeader>
-          <CardTitle className="text-xl">Welcome Back!</CardTitle>
-          <CardDescription>
-            Login with your GitHub or Email Account
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <Button disabled={githubPending} onClick={signInWithGithub} className="w-full" variant="outline">
-           {
-              githubPending ? (
-                  <>
-                  <Loader className="size-4 animate-spin" />
-                  <span>Loading...</span>
-                  </>
-              ) : (
-                 <>
-                  <GithubIcon className="size-4" /> 
-                  Sign in with GitHub
-                 </>
-              )
-           }
-          </Button>
-          <div className="relative flex items-center text-center text-sm">
-            <div className="flex-grow border-t border-border"></div>
-            <span className="relative z-10 bg-card px-2 text-muted-foreground">
-              or continue with
-            </span>
-            <div className="flex-grow border-t border-border"></div>
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Welcome Back!</CardTitle>
+        <CardDescription>
+          Login with your GitHub or Email Account
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <Button
+          disabled={githubPending}
+          onClick={signInWithGithub}
+          className="w-full"
+          variant="outline"
+        >
+          {githubPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Sign in with GitHub
+            </>
+          )}
+        </Button>
+        <div className="relative flex items-center text-center text-sm">
+          <div className="flex-grow border-t border-border"></div>
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">
+            or continue with
+          </span>
+          <div className="flex-grow border-t border-border"></div>
+        </div>
+
+        <div className="grid gap-3">
+          <div className="grid gap-2 ">
+            <Label htmlFor="email">Email</Label>
+            <Input value={email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="ajji@gmail.com" required />
           </div>
-  
-      <div className="grid gap-3">
-      <div className="grid gap-2 ">
-          <Label htmlFor="email">
-              Email
-          </Label>
-          <Input type="email" placeholder="ajji@gmail.com" />
-      </div> 
-      </div>
-  
-      <Button>
-          Continue with Email
-      </Button>
-  
-        </CardContent>
-      </Card>
-    )
+        </div>
+
+        <Button onClick={signInWithEmail} disabled={emailPending}>
+            {
+                emailPending ? (
+                    <>
+                    <Loader2 className="size-4 animate-spin" />
+                    <span>Loading...</span>
+                    </>
+                ) : (
+               <>
+               <Send className="size-4" />
+               <span>Continue with Email</span>
+               </>
+                )
+            }
+        </Button>
+      </CardContent>
+    </Card>
+  );
 }
--- edu-global/lib/auth.ts
+++ edu-global/lib/auth.ts
@@ -1,8 +1,11 @@
 import { prisma } from './db';
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { emailOTP } from "better-auth/plugins"
+
 
 import { env } from "./env";
+import { resend } from './resend';
 
 
 
@@ -18,5 +21,19 @@ export const auth = betterAuth({
             clientId: env.AUTH_GITHUB_CLIENT_ID ,
             clientSecret: env.AUTH_GITHUB_SECRET,
         } 
-    }
+    },
+
+    plugins: [
+        emailOTP({ 
+            async sendVerificationOTP({ email, otp }) { 
+                const {data,error} = await resend.emails.send({
+                    from: "Edu Global <onboarding@resend.dev>",
+                    to: [email],
+                    subject: "Edu Global - Verify your email ",
+                    html: `<p>Your otp is <strong>${otp}</strong></p>`
+                }) 
+            }, 
+        }) 
+    ]
+
 });
